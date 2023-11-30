deb7da419142ff1f231ac318ef9f2af0031df626
Sebastian <sebastian.sarviala@gmail.com>
2023-11-30 12:48:34 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+};
+
+multiplicator('This is not a number', 4, 'The product of a string and 4 is');
