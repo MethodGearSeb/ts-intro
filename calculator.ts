9366280717f479f15dd6429170f0397a6a718edf
Sebastian <sebastian.sarviala@gmail.com>
2023-12-08 11:38:38 +0200

    Endpoint 'calculate' handles POST request without causing Eslint errors

--- calculator.ts
+++ calculator.ts
@@ -1,6 +1,6 @@
-type Operation = 'multiply' | 'add' | 'divide';
+export type Operation = 'multiply' | 'add' | 'divide';
 
-const calculator = (a: number, b: number, op: Operation): number => {
+export const calculator = (a: number, b: number, op: Operation): number => {
   switch (op) {
     case 'add':
       return a + b;
@@ -14,12 +14,14 @@ const calculator = (a: number, b: number, op: Operation): number => {
   }
 };
 
-try {
-  console.log(calculator(1, 5, 'divide'));
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong: ';
-  if (error instanceof Error) {
-    errorMessage += error.message;
+if (process.argv.find((arg) => arg.includes('calculator'))) {
+  try {
+    console.log(calculator(1, 5, 'divide'));
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong: ';
+    if (error instanceof Error) {
+      errorMessage += error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- index.ts
+++ index.ts
@@ -1,12 +1,33 @@
 import express from 'express';
+import { calculator, Operation } from './calculator';
+
 const app = express();
+const PORT = 3003;
+const BASE_URL = `http://localhost:${PORT}`;
+
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3003;
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if (
+    !value1 ||
+    !value2 ||
+    !op ||
+    isNaN(Number(value1)) ||
+    isNaN(Number(value2))
+  )
+    res.status(400).send({ error: 'malformatted request' });
+
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+  res.send(result);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(BASE_URL + '/ping');
 });
